61c8a79db5f9cd1ad80446f18d8e92c7823f3682
EzraHaleva <halevaezra@gmail.com>
2020-02-07 13:37:48 -0500

    First draft of TeamMember and Team classes

--- src/Team.java
+++ src/Team.java
@@ -12,40 +12,68 @@ public class Team
    public Team()
    {
       //this is the default constructor
+      this.team = new TeamMember[GROW_SIZE];
+      this.numMembers = 0;
    }
    
    private int find(TeamMember m)
    {
-       
+      for(int i = 0; i < numMembers; i++)
+         if(team[i].equals(m))
+            return i;
+      return NOT_FOUND;
    }
    
    private void grow()
    {
-       
+      TeamMember[] newTeamArray = TeamMember[team.size * GROW_SIZE];
+      copyTeamToNewArray(newTeamArray);
+      this.team = newTeamArray;
    }
    
+   private void copyTeamToNewArray(TeamMember[] newArray){
+      for(int i = 0; i < numMembers; i++)
+         newArray[i] = team[i];
+   }
+
    public boolean isEmpty()
    {
-       
+      if(numMembers == 0)
+         return true;
+      return false; 
    }
    
    public void add(TeamMember m)
    {     
-            
+      team[numMembers] = m;
+      numMembers++;
+      if(numMembers == team.size)
+         grow();
    }
    
    public boolean remove(TeamMember m)
    {
-       
-   } 
+      int indexToRemove = find(m);
+      team[indexToRemove] = team[numMembers-1];
+      team[numElements-1] = null;
+      numElements--;
+   }
    
    public boolean contains(TeamMember m)
    {
-      
+      for(int i = 0; i < numMembers; i++)
+         if(team[i].equals(m))
+            return true;
+      return false;
    } 
    
    public void print()
    {
-      //set up a for loop and call the toString() method
+      for(int i = 0; i < numMembers; i++)
+         System.out.println(team[i].toString());
    } 
+   
+   public static void main(String[] args){
+      //testbed main
+   }
 }
