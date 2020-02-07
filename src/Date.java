f6ac6768d314ead817943b9355f5d6be10f61cca
Robert <robert.bonagura@gmail.com>
2020-02-07 12:34:34 -0500

    Implement equals method

--- src/Date.java
+++ src/Date.java
@@ -1,82 +1,81 @@
+import java.util.StringTokenizer;
 /**
-  
+ This class defines the properties of a date.
  @author Robert Bonagura
  */
-public class Date 
-{
-   private int  day;
-   private int  month;
-   private int  year;
-   
-   public Date(String d)
-   {
+public class Date {
+   private int day;
+   private int month;
+   private int year;
+
+   /**
+    * Tokenize String into a Date object.
+    * @param d String to be parsed into date.
+    */
+   public Date(String d) {
       //use StringTokenizer to parse the String and create a Date object
       String delim = "/";
       StringTokenizer st = new StringTokenizer(d, delim);
       this.month = Integer.parseInt(st.nextToken());
-      if (st.hasMoreTokens()){
+      if (st.hasMoreTokens()) {
          this.day = Integer.parseInt(st.nextToken());
       }
-      if (st.hasMoreTokens()){
-         this.year = Integer.parseInt(st.nextToken().parseInt());
+      if (st.hasMoreTokens()) {
+         this.year = Integer.parseInt(st.nextToken());
       }
       return;
    }
-   
-   public Date(Date d)
-   {
+
+   public Date(Date d) {
       //this is a constructor
       this.day = d.day;
       this.month = d.month;
       this.year = d.year;
       return;
-   }      
-   
-   public boolean isValid()
-   {
+   }
+
+   public boolean isValid() {
       int DAY_FIRST = 1;
-      switch (this.month){
-         case JAN:
-         case MAR:
-         case MAY:
-         case JUL:
-         case AUG:
-         case OCT:
-         case DEC:
-            if (this.day < DAY_FIRST || this.day > DAYS_ODD){
+      switch (this.month) {
+         case Month.JAN:
+         case Month.MAR:
+         case Month.MAY:
+         case Month.JUL:
+         case Month.AUG:
+         case Month.OCT:
+         case Month.DEC:
+            if (this.day < DAY_FIRST || this.day > Month.DAYS_ODD) {
                return false;
             }
             break;
-         case APR:
-         case JUN:
-         case SEP:
-         case NOV:
-            if (this.day < DAY_FIRST || this.day > DAYS_EVEN){
+         case Month.APR:
+         case Month.JUN:
+         case Month.SEP:
+         case Month.NOV:
+            if (this.day < DAY_FIRST || this.day > Month.DAYS_EVEN) {
                return false;
             }
             break;
-         case FEB:
-            if (isLeapYear()){
-               if (this.day < DAY_FIRST || this.day > DAYS_FEB + 1) {
+         case Month.FEB:
+            if (isLeapYear()) {
+               if (this.day < DAY_FIRST || this.day > Month.DAYS_FEB + 1) {
                   return false;
-               }   
+               }
             } else {
-               if (this.day < DAY_FIRST || this.day > DAYS_FEB) {
+               if (this.day < DAY_FIRST || this.day > Month.DAYS_FEB) {
                   return false;
-               } 
-            } 
+               }
+            }
             break;
          default:
             return false;
-            break;
       }
       return true;
    }
 
-   private void isLeapYear()
-   {
+   private boolean isLeapYear() {
       if (this.year % 4 == 0) {
-         if (this.year % 100 == 0){
+         if (this.year % 100 == 0) {
             if (this.year % 400 == 0) {
                return true;
             } else {
@@ -88,25 +87,27 @@ public class Date
       }
       return false;
    }
-   
+
    @Override
-   public String toString()
-   {
-       //use the format "month/day/year"
-       String slash = "/";
-       String monthStr = Integer.toString(this.month);
-       String dayStr = Integer.toString(this.day);
-       String yearStr = Integer.toString(this.year);
-       String output = monthStr + slash + dayStr + slash + yearStr;
-       return output;
+   public String toString() {
+      String slash = "/";
+      String monthStr = Integer.toString(this.month);
+      String dayStr = Integer.toString(this.day);
+      String yearStr = Integer.toString(this.year);
+      String output = monthStr + slash + dayStr + slash + yearStr;
+      return output;
 
    }
-   
+
    @Override
-   public boolean equals(Object obj)
-   {
-    
-    
-   }  
-
-
+   public boolean equals(Object obj) {
+      if (obj instanceof Date) {
+         Date date = (Date) obj;
+         if (this.day == date.day && this.month == date.month
+                 && this.year == date.year) {
+            return true;
+         }
+      }
+      return false;
+   }
+}
